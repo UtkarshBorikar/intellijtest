934c609cfb9a9f7c818288366e597a510b56565c
UtkarshBorikar <66049600+UtkarshBorikar@users.noreply.github.com>
2021-10-07 16:33:18 -0400

    Initial commit

---
+++ src/main/java/com/example/javafxlearn/HelloApplication.java
@@ -0,0 +1,37 @@
+package com.example.javafxlearn;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root, 600,600,Color.LIGHTSKYBLUE);
+
+        Image icon = new Image("sasukeitachi.jpg");
+        stage.getIcons().add(icon);
+        stage.setTitle("Hi");
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
